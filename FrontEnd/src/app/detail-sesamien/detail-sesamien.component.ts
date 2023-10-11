29b1b08bb08f951b81058f1a5292c05af6dbc174
TsioryFano <tsioryfano@hotmail.com>
2023-10-11 08:11:07 +0300

    Suppression du fichier /detail-sesamien.component.css

--- FrontEnd/src/app/detail-sesamien/detail-sesamien.component.ts
+++ FrontEnd/src/app/detail-sesamien/detail-sesamien.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-detail-sesamien',
-  templateUrl: './detail-sesamien.component.html',
-  styleUrls: ['./detail-sesamien.component.css']
+  templateUrl: './detail-sesamien.component.html'
 })
 export class DetailSesamienComponent {
 
